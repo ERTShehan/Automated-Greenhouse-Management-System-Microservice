2e11872fe317c4c28fc44b131d6332063914d4dc
Shehan <ertsshehan@gmail.com>
2026-04-09 06:25:21 +0530

    enable service discovery and Feign clients in ZoneServiceApplication

--- zone-service/src/main/java/lk/ijse/zoneservice/ZoneServiceApplication.java
+++ zone-service/src/main/java/lk/ijse/zoneservice/ZoneServiceApplication.java
@@ -2,8 +2,12 @@ package lk.ijse.zoneservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ZoneServiceApplication {
 
     public static void main(String[] args) {
