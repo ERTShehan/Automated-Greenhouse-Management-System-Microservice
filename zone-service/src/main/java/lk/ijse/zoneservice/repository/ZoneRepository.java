17373a9270a6893f99f7473d46050158d0df3883
Shehan <ertsshehan@gmail.com>
2026-04-09 18:33:36 +0530

    add ZoneRepository interface for data access

---
+++ zone-service/src/main/java/lk/ijse/zoneservice/repository/ZoneRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.zoneservice.repository;
+
+import lk.ijse.zoneservice.entity.Zone;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface ZoneRepository extends JpaRepository<Zone, String> {
+}
