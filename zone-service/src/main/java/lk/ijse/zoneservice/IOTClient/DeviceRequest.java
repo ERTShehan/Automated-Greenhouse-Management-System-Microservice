2c73b1ba94ca610e27a7f3ec170ef43c85828f80
Shehan <ertsshehan@gmail.com>
2026-04-10 07:55:20 +0530

    add DeviceRequest and DeviceResponse classes for device management add IotClient interface for interacting with the IoT API

---
+++ zone-service/src/main/java/lk/ijse/zoneservice/IOTClient/DeviceRequest.java
@@ -0,0 +1,16 @@
+package lk.ijse.zoneservice.IOTClient;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class DeviceRequest {
+    private String zoneId;
+    private String name;
+    private String type;
+}
