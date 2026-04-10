0aaea684e1a555acc65449e61d189cd378ea4f9a
Shehan <ertsshehan@gmail.com>
2026-04-10 08:00:07 +0530

    add ZoneRequest and ZoneResponse classes for zone data transfer

---
+++ zone-service/src/main/java/lk/ijse/zoneservice/dto/ZoneRequest.java
@@ -0,0 +1,22 @@
+package lk.ijse.zoneservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ZoneRequest {
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Double minTemp;
+
+    @NotNull
+    private Double maxTemp;
+}
